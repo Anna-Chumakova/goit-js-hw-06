5f7d1f9b63b7f84e26830ff9b1e9229b9763e961
Anna-Chumakova <ann020187@gmail.com>
2022-07-05 11:42:30 +0300

    task2 правки

--- js/task-02.js
+++ js/task-02.js
@@ -7,26 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 const ingredientsEl = document.querySelector("ul");
-const listElFirst = document.createElement("li");
-listElFirst.classList.add("item");
-listElFirst.textContent = 'Potatoes';
-const listElSecond = document.createElement("li");
-listElSecond.textContent = 'Mushrooms';
-listElSecond.classList.add("item");
-const listElThird = document.createElement("li");
-listElThird.textContent = 'Garlic';
-listElThird.classList.add("item");
-const listElfourth = document.createElement("li");
-listElfourth.textContent = 'Tomatos';
-listElfourth.classList.add("item");
-const listElFifth = document.createElement("li");
-listElFifth.textContent = 'Herbs';
-listElFifth.classList.add("item");
-const listElSixth = document.createElement("li");
-listElSixth.textContent = 'Condiments';
-listElSixth.classList.add("item");
+const createList = ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  ingredientsEl.append(item);
+})
 
-ingredientsEl.append(listElFirst, listElSecond, listElThird, listElfourth, listElFifth, listElSixth);
+
 
 //const createList = (ingredients) => {
 //  return ingredients.reduce((acc, ingredient) => acc + `<li>${ingredient}</li>`, "");
