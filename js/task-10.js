d84b8fd5b22965e58ebd538149c2b0a53021c114
Anna-Chumakova <ann020187@gmail.com>
2022-07-10 18:51:07 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -2,42 +2,36 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const fragment = document.querySelector("#boxes");
+const divEl = document.querySelector("#boxes");
 const inputEl = document.querySelector('input[type="number"]');
 const btnElCreate = document.querySelector('button[data-create]');
 const btnElDestroy = document.querySelector('button[data-destroy]');
 
-inputEl.addEventListener("input", onInput);
 btnElCreate.addEventListener("click", onCreateBoxes);
 btnElDestroy.addEventListener("click", onBoxDestroy);
+ 
+const valueSize = 20;
 
+function onCreateBoxes() {
+  let fragment = new DocumentFragment();
+   // let arrayDiv = [];
+  for (let i = 1; i <= inputEl.value; i += 1) {
+  
+  const divNew = document.createElement('div');
+  
+  divNew.style.width = `${valueSize + i * 10}px`;
+  divNew.style.height = `${valueSize + i * 10}px`;
+  divNew.style.backgroundColor = getRandomHexColor();
+  //arrayDiv.push(divNew[i]);  
+    
+    fragment.appendChild(divNew);
+  }
+  divEl.append(fragment);
+  }
 
-function onBoxDestroy () {
-  fragment.innerHTML = '';
+ function onBoxDestroy () {
+  divEl.innerHTML = '';
   inputEl.value = '';
 }
- function onInput(elem) {
-   let numberArray = [];
-   const item = elem.currentTarget.value;
-   numberArray.push(item);
-   console.log(numberArray);
- }
-function onCreateBoxes() {
-
-  let numberArray = [];
-  for (let i = 1; i <= inputEl.value; i += 1) {
-    numberArray.push(i);
-  };
-  console.log(numberArray);
-  const boxes = numberArray.map((number) => {
-    const createBox  = document.createElement('div');
-    const valueSize = 30 + number * 10 + 'px';
-    createBox.style.width = valueSize;
-    createBox.style.height = valueSize;
-    createBox.style.backgroundColor = getRandomHexColor();
-    return createBox;
-    }); 
-  fragment.append(...boxes);
-};
 
 
